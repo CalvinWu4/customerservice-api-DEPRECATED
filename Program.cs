c53c920780bf29618c81a4a5d9bb42b5930c7a64
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-02-26 09:12:43 -0500

    updated project from mvc to api

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace CustomerSupport
+namespace CustomerServiceAPI
 {
     public class Program
     {
