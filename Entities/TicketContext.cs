940ec5670100980efc06ab30940356d34b1a7bb3
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-02-27 11:06:14 -0500

    added basic ticket dto and ticket context

---
+++ Entities/TicketContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using CustomerServiceAPI.Models;
+
+namespace CustomerServiceAPI.Entities
+{
+    public class TicketContext : DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TicketDto> Tickets { get; set; }
+    }
+}
---
+++ Models/TicketDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CustomerServiceAPI.Models
+{
+    public class TicketDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+    }
+}
