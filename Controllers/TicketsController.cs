314aceed618000429c3c3725f8d2a394f3c97199
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-02-27 23:04:40 -0500

    updated ticket controller comments

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -7,8 +7,6 @@ using CustomerServiceAPI.Models;
 using CustomerServiceAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace CustomerServiceAPI.Controllers
 {
     [Route("api/[controller]")]
@@ -21,7 +19,7 @@ namespace CustomerServiceAPI.Controllers
             _ticketRepository = ticketRepository;
         }
 
-        // GET: api/values
+        // GET: api/tickets
         [HttpGet]
         public IActionResult Get()
         {
@@ -31,7 +29,7 @@ namespace CustomerServiceAPI.Controllers
             return Ok(results);
         }
 
-        // GET api/values/5
+        // GET api/tickets/5
         [HttpGet("{id}", Name = "GetTicket")]
         public IActionResult Get(int id)
         {
@@ -44,7 +42,7 @@ namespace CustomerServiceAPI.Controllers
             return Ok(ticket);
         }
 
-        // POST api/values
+        // POST api/tickets
         [HttpPost]
         public IActionResult Post([FromBody]TicketForCreationDto ticket)
         {
@@ -66,13 +64,13 @@ namespace CustomerServiceAPI.Controllers
             return CreatedAtRoute("GetTicket", new { id = createdTicket.Id }, createdTicket);
         }
 
-        // PUT api/values/5
+        // PUT api/tickets/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/tickets/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
