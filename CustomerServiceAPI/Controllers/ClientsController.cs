0768c64d33816d7786e75d5a449e6aef7f48c2e0
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-03-30 19:00:10 -0400

    added GET /clients

--- CustomerServiceAPI/Controllers/ClientsController.cs
+++ CustomerServiceAPI/Controllers/ClientsController.cs
@@ -25,9 +25,9 @@ namespace CustomerServiceAPI.Controllers
 
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(_clientRepository.GetClients());
         }
 
         // GET api/values/5
@@ -65,12 +65,14 @@ namespace CustomerServiceAPI.Controllers
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            
         }
     }
 }
